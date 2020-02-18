b55b2b1bb1cf9a77462f117ba99ba1c7695525dc
kian behzad <kian.behzad@gmail.com>
2020-02-18 22:46:45 +0330

    [feature]implement referee

---
+++ src/referee.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.insert(1, '../protoCompiled')
+
+from protoCompiled.SIM2REF import packet_pb2
+from src.firasimClient import FIRASimClient
+from src.firasimServer import FIRASimServer
+from src.common import WorldModel
+
+
+class Referee():
+    def __init__(self):
+        self.firasimserver = FIRASimServer('127.0.0.1', 50054)
+        self.firasimclient = FIRASimClient('127.0.0.1', 50051)
+
+        self.worldmodel = WorldModel()
+
+        self.firasimserver.start_receiveing(self.vision_detection)
+
+    def vision_detection(self, data):
+        environment = packet_pb2.Environment()
+        environment.ParseFromString(data)
+        self.worldmodel.update_worldmodel(environment)
+
+
+
+if __name__ == "__main__":
+    referee = Referee()
