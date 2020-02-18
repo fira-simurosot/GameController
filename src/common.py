77246f7dcc5a0c2a7024e469a42e2443efba7504
kian behzad <kian.behzad@gmail.com>
2020-02-18 21:54:47 +0330

    [feature] imlement ball and robot models in python

---
+++ src/common.py
@@ -0,0 +1,14 @@
+class Robot():
+    def __init__(self):
+        self.id = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.orientation = 0.0
+        self.isYellow = True
+        self.wheel_right = 0.0
+        self.wheel_left = 0.0
+
+class Ball():
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
