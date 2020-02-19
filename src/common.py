089b3eef1b1bae072f40fb050d52860cc9e42825
kian behzad <kian.behzad@gmail.com>
2020-02-19 16:40:39 +0330

    [converter] add convert functions in common

--- src/common.py
+++ src/common.py
@@ -1,4 +1,5 @@
-from protoCompiled.SIM2REF import packet_pb2
+from protoCompiled import common_pb2
+from protoCompiled.SIM2REF import packet_pb2, replacement_pb2, command_pb2
 from enum import Enum
 
 class Robot():
@@ -73,4 +74,38 @@ class GameState():
     def __init__(self):
         self.state = GameStateEnum.Halt
         self.actor = ActorEnum.NoOne
+
+class Converter():
+    def __init__(self):
+        pass
+
+    def convert_protocommand_to_Robot(self, command, isYellow):
+        robots = []
+        for robot in command.wheels:
+            robottmp = Robot()
+            robottmp.id = robot.robot_id
+            robottmp.isYellow = isYellow
+            robottmp.wheel_left = robot.left
+            robottmp.wheel_right = robot.right
+            robots.append(robottmp)
+        return robots
+
+    def convert_protoRobots_to_Robot(self, protoRobots, isYellow):
+        robots = []
+        for robot in protoRobots.robots:
+            robottmp = Robot()
+            robottmp.id = robot.robot_id
+            robottmp.isYellow = isYellow
+            robottmp.x = robot.x
+            robottmp.y = robot.y
+            robottmp.orientation = robot.orientation
+            robots.append(robottmp)
+        return robots
+
+    def convert_protoBall_to_Ball(self, protoball):
+        ball = Ball()
+        ball.x = protoball.x
+        ball.y = protoball.y
+        ball.z = protoball.z
+        return ball
 
--- src/firasimClient.py
+++ src/firasimClient.py
@@ -4,7 +4,9 @@ sys.path.insert(1, '../protoCompiled')
 import socket
 from protoCompiled import common_pb2
 from protoCompiled.SIM2REF import packet_pb2, replacement_pb2, command_pb2
+from protoCompiled.REF2CLI import messages_pb2
 from src.common import Robot, Ball
+from src.common import Converter
 
 
 class FIRASimClient():
@@ -16,17 +18,6 @@ class FIRASimClient():
                              socket.SOCK_DGRAM)  # UDP
         # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-
-    def convert_protocommand_to_Robot(self, command, isYellow):
-        robots = []
-        for robot in command.wheels:
-            robottmp = Robot()
-            robottmp.id = robot.robot_id
-            robottmp.isYellow = isYellow
-            robottmp.wheel_left = robot.left
-            robottmp.wheel_right = robot.right
-            robots.append(robottmp)
-        return robots
 
     def send_robot_command(self, robotsYellow, robotsBlue):
         commands = command_pb2.Commands()
@@ -49,17 +40,6 @@ class FIRASimClient():
         # print(packet)
         self.sock.sendto(packet.SerializeToString(), (self.ip, self.port))
 
-    def convert_protoRobots_to_Robot(self, protoRobots, isYellow):
-        robots = []
-        for robot in protoRobots.robots:
-            robottmp = Robot()
-            robottmp.id = robot.robot_id
-            robottmp.isYellow = isYellow
-            robottmp.x = robot.x
-            robottmp.y = robot.y
-            robottmp.orientation = robot.orientation
-            robots.append(robottmp)
-        return robots
 
     def send_robot_replacement(self, robotsYellow, robotsBlue):
         replacement = replacement_pb2.Replacement()
@@ -85,13 +65,6 @@ class FIRASimClient():
         packet.replace.CopyFrom(replacement)
         self.sock.sendto(packet.SerializeToString(), (self.ip, self.port))
 
-    def convert_protoBall_to_Ball(self, protoball):
-        ball = Ball()
-        ball.x = protoball.x
-        ball.y = protoball.y
-        ball.z = protoball.z
-        return ball
-
     def send_ball_replacement(self, ball):
         replacement = replacement_pb2.Replacement()
         replacement.ball.x = ball.x
@@ -106,18 +79,19 @@ class FIRASimClient():
 
 if __name__ == "__main__":
     cli = FIRASimClient('127.0.0.1', 50051)
+    conv = Converter()
 
-    # command = messages_pb2.Command()
-    # for i in range(5):
-    #     wheelspeed = command.wheels.add()
-    #     wheelspeed.robot_id = i
-    #     wheelspeed.right = 10
-    #     wheelspeed.left = -10
-    # yelbot = cli.convert_protocommand_to_Robot(command, True)
-    # blubot = cli.convert_protocommand_to_Robot(command, False)
-    #
-    # while True:
-    #     cli.send_robot_command(yelbot, blubot)
+    command = messages_pb2.Command()
+    for i in range(5):
+        wheelspeed = command.wheels.add()
+        wheelspeed.robot_id = i
+        wheelspeed.right = 10
+        wheelspeed.left = -10
+    yelbot = conv.convert_protocommand_to_Robot(command, True)
+    blubot = conv.convert_protocommand_to_Robot(command, False)
+
+    while True:
+        cli.send_robot_command(yelbot, blubot)
 
     # robots = messages_pb2.Robots()
     # for i in range(5):
@@ -126,14 +100,14 @@ if __name__ == "__main__":
     #     robot.x = 1
     #     robot.y = 1
     #     robot.orientation = 0
-    # yelbot = cli.convert_protoRobots_to_Robot(robots, True)
-    # blubot = cli.convert_protoRobots_to_Robot(robots, False)
+    # yelbot = conv.convert_protoRobots_to_Robot(robots, True)
+    # blubot = conv.convert_protoRobots_to_Robot(robots, False)
     # while True:
     #     cli.send_robot_replacement(yelbot, blubot)
 
-    protoball = common_pb2.Ball()
-    protoball.x = 0.5
-    protoball.y = 0.5
-    ball = cli.convert_protoBall_to_Ball(protoball)
-    while True:
-        cli.send_ball_replacement(ball)
+    # protoball = common_pb2.Ball()
+    # protoball.x = 0.5
+    # protoball.y = 0.5
+    # ball = conv.convert_protoBall_to_Ball(protoball)
+    # while True:
+    #     cli.send_ball_replacement(ball)
